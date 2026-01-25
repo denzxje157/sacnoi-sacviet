9de2113225f54c8f310d686a883eb9baece2aa20
denzxje157 <minhminh887701@gmail.com>
2026-01-26 00:53:39 +0700

    huhu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar: React.FC = () => {
           {/* Logo */}
           <Link to="/" className="flex items-center gap-3 group">
             <img 
-              src={`${import.meta.env.BASE_URL}/pictures/logo.png`} 
+              src={`${import.meta.env.BASE_URL}/pictures-54dantoc/logo.png`} 
               alt="Logo Sắc Nối" 
               className="h-12 w-12 rounded-full object-cover border-2 border-gold shadow-md transition-transform group-hover:scale-110"
             />
