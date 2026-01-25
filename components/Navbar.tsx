056656e5d07c5acb3cd17b01d550cc87a2f104af
denzxje157 <minhminh887701@gmail.com>
2026-01-25 23:56:17 +0700

    update lan cuii hiuhiu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar: React.FC = () => {
           {/* Logo */}
           <Link to="/" className="flex items-center gap-3 group">
             <img 
-              src="pictures/logo.png" 
+              src="/pictures/logo.png" 
               alt="Logo Sắc Nối" 
               className="h-12 w-12 rounded-full object-cover border-2 border-gold shadow-md transition-transform group-hover:scale-110"
             />
