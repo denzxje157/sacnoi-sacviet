88f0008def92e43e99b60657cdd7766697bd2b3f
denzxje157 <minhminh887701@gmail.com>
2026-02-11 12:45:07 +0700

    update file pictures

---
+++ data/siteData.ts
@@ -0,0 +1,4 @@
+export { ethnicData } from "../pages/Home";
+export { heritageData } from "../pages/MapPage";
+export { marketplaceData } from "../pages/Marketplace";
+export { libraryData } from "../pages/Library";
